80d806ae5ab495452b8259e62280a9b212532572
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2023-06-09 16:26:22 +0300

    change version function Fixes #126

--- server/src/api/version.ts
+++ server/src/api/version.ts
@@ -1,14 +1,30 @@
 import { Request, Response } from 'express';
-//import { model, Schema, Model, Document, Mongoose, connect, Types } from 'mongoose';
+import { model, Schema, Model, Document, Mongoose, connect, Types } from 'mongoose';
 import PlutchikError from '../model/error';
-//import User from "../model/user";
 import colours from "../model/colours";
-import version_file  from "../version.json";
+import MongoProto from '../model/mongoproto';
+var npm_package_version = require('../../package.json').version;
+const mongoVersion = model<{
+    version: string
+}>('version', new Schema({
+    version: {
+        type: String, 
+        required: true
+    }
+}));
+
 export default async function version(c: any, req: Request, res: Response) {
-//    const factoryid = new Types.ObjectId(c.request.params["factoryid"]);
     console.log(`${colours.fg.green}API: version function${colours.reset}`);
+    let v = '0.0.0';
     try {
-        return res.status(200).json(version_file);
+        MongoProto.connectMongo();
+        const mv = await mongoVersion.find();
+        if (mv.length === 1) v = mv[0].version;
+        return res.status(200).json({
+            api: npm_package_version,
+            data: v,
+            ai: '0.0.0'
+        });
     } catch (e: any) {
         return res.status(400).json(e);
     }
