0eca255692295367f55fb5d438ab8b5de0c864a1
kmvpvl <43674119+kmvpvl@users.noreply.github.com>
2023-06-25 19:12:46 +0300

    add: pending type shattle

--- client/src/components/pending/pending.tsx
+++ client/src/components/pending/pending.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import './pending.css';
+type PendingType = "squares" | "shattle";
 type IPendingProps = {
     color?: string;
+    type?: PendingType;
 }
 
 type IPendingState = {
@@ -10,7 +12,7 @@ type IPendingState = {
 
 export default class Pending extends React.Component <IPendingProps, IPendingState>{
     state: IPendingState = {
-        useCount:0
+        useCount:0,
     }
     incUse(){
         const nState: IPendingState = this.state;
@@ -23,18 +25,29 @@ export default class Pending extends React.Component <IPendingProps, IPendingSta
         this.setState(nState);
     }
     render(): React.ReactNode {
-        return <>
-            {this.state.useCount > 0?<div className='pending-container'>
-                <span className='pending-square' style={{transformOrigin:'left top'}}></span>
-                <span className='pending-square' style={{transformOrigin:'center top'}}></span>
-                <span className='pending-square' style={{transformOrigin:'right top'}}></span>
-                <span className='pending-square' style={{transformOrigin:'left center'}}></span>
-                <span className='pending-square' style={{transformOrigin:'center center'}}></span>
-                <span className='pending-square' style={{transformOrigin:'right center'}}></span>
-                <span className='pending-square' style={{transformOrigin:'left bottom'}}></span>
-                <span className='pending-square' style={{transformOrigin:'center bottom'}}></span>
-                <span className='pending-square' style={{transformOrigin:'right bottom'}}></span>
-            </div>:<></>}
-        </>;
+        if (this.state.useCount === 0) return <></>;
+        switch(this.props.type) {
+            case "shattle":
+                return <>
+                <div className='pending-shattle-container'>
+                    <span className='pending-shattle-point'></span>
+                </div>
+                </>;
+            case 'squares':
+            default:
+                return <>
+                <div className='pending-squares-container'>
+                    <span className='pending-squares-square' style={{transformOrigin:'left top'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'center top'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'right top'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'left center'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'center center'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'right center'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'left bottom'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'center bottom'}}></span>
+                    <span className='pending-squares-square' style={{transformOrigin:'right bottom'}}></span>
+                </div>
+            </>;
+        }
     }
 }
