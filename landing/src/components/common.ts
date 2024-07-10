da395ecc213f6748ee36b24803497428425fd5be
Pavel Komov <kmvpvl@gmail.com>
2024-07-10 16:24:48 +0300

    deleting landing which integrated to client

--- landing/src/components/common.ts
+++
@@ -1,3 +0,0 @@
-export type Emotion = "joy" | "trust" |  "fear" | "surprise" | "disgust" | "sadness" | "anger" | "anticipation";
-
-export const Emotions = ["joy", "trust", "fear", "surprise", "disgust", "sadness", "anger", "anticipation"];
